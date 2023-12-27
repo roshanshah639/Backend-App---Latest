e5519b1f32f05a51a595ccd07663490a1595d50c
Roshan Shah <roshanshah639@gmail.com>
2023-12-27 07:58:06 +0545

    multer & middleware setup

---
+++ src/utlis/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // file has been uploaded on cloudinary
+    console.log("File uploaded on cloudinary successfully.", response.url);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the temp file from local storage as file was not uploaded on cloudinary
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
