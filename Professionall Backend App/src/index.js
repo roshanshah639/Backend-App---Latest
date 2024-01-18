093cd8ac99a956856bf946bd97f493154b38b2b7
Roshan Shah <roshanshah639@gmail.com>
2024-01-18 19:20:12 +0545

    utils code

--- Professionall Backend App/src/index.js
+++ Professionall Backend App/src/index.js
@@ -1,29 +1,18 @@
 import dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
-
-/*
-import express from "express";
-const app = express();
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.error("MongoDB Connection Error", error);
-      throw error;
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is Running on Port ${process.env.PORT}`);
     });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("MongoDB Connection Error", error);
-    throw error;
-  }
-})();
-*/
+    console.log("MongoDB Connected Successfully!!");
+  })
+  .catch((err) => {
+    console.error("MongoDB Connection Failed!!", err);
+  });
